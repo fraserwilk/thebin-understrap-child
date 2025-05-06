bacb382234fb4293f80448a0cdfdcd17d0ed06d4
Fraser Wilkinson <fraserwilk@users.noreply.github.com>
2025-05-06 16:34:17 +1000

    event featured img

--- loop-templates/content-event.php
+++ loop-templates/content-event.php
@@ -12,31 +12,31 @@ defined('ABSPATH') || exit;
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<div class="row align-items-center border border-light">
-	<!-- Left Column: Title & Date -->
-	<div class="col-md-6">
-		<h1 class="entry-title">
-				<?php the_field('event_title'); ?>
-		</h1>
-		<p class="text-muted mb-0">
-			<strong>Date: </strong>
-			<?php the_field('event_start_date'); ?>
-			<?php if (get_field('event_end_date')) : ?>
-				<span> to <?php the_field('event_end_date'); ?></span>
+		<!-- Left Column: Title & Date -->
+		<div class="col-md-6">
+			<h1 class="entry-title">
+					<?php the_field('event_title'); ?>
+			</h1>
+			<p class="text-muted mb-0">
+				<strong>Date: </strong>
+				<?php the_field('event_start_date'); ?>
+				<?php if (get_field('event_end_date')) : ?>
+					<span> to <?php the_field('event_end_date'); ?></span>
+				<?php endif; ?>
+			</p>
+
+			<p><strong>Location:</strong> <?php the_field('event_location'); ?></p>
+			<p><strong>Website:</strong> <a href="<?php the_field('event_website'); ?>" target="_blank"><?php the_field('event_website'); ?></a></p>
+		</div>
+
+		<!-- Right Column: Image -->
+		<div class="col-md-6">
+			<?php if (has_post_thumbnail()): ?>
+				<a href="<?php the_permalink(); ?>" class="cat-img">
+					<?php the_post_thumbnail('medium', ['class' => 'img-fluid']); ?>
+				</a>
 			<?php endif; ?>
-		</p>
-
-		<p><strong>Location:</strong> <?php the_field('event_location'); ?></p>
-		<p><strong>Website:</strong> <a href="<?php the_field('event_website'); ?>" target="_blank"><?php the_field('event_website'); ?></a></p>
-	</div>
-
-	<!-- Right Column: Image -->
-	<div class="col-md-6">
-		<?php if (has_post_thumbnail()): ?>
-			<a href="<?php the_permalink(); ?>">
-				<?php the_post_thumbnail('medium_large', ['class' => 'img-fluid']); ?>
-			</a>
-		<?php endif; ?>
-	</div>
+		</div>
 	</div>
 
 
